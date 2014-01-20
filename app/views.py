7a2ad01ae13da540c9e7d2a6d27584304eec8513
Shivam Bansal <shivam5992@gmail.com>
2014-01-20 18:57:18 +0530

    follow and unfollow user functionality added

--- app/views.py
+++ app/views.py
@@ -37,8 +37,10 @@ def after_login(resp):
 			nickname = resp.email.split('@')[0]
 		nickname = User.make_unique_nickname(nickname)
 		user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
-		user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
 		db.session.add(user)
+		db.session.commit()
+		# make the user follow him
+		db.session.add(user.follow(user))
 		db.session.commit()
 	remember_me = False
 	if 'remember_me' in session:
@@ -123,6 +125,45 @@ def edit():
 		form.about_me.data = g.user.about_me
 	return render_template('edit.html',
 		form = form)
+
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+	user = User.query.filter_by(nickname = nickname).first()
+	if user == None:
+		flash('User' + nickname + 'Not Found')
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('Yoc cannot follow yourself')
+		return redirect(url_for('user', nickname = nickname))
+	u = g.user.follow(user)
+	if u is None:
+		flash('cannot follow' + nickname)
+		return redirect(url_for('user', nickname = nickname))
+	db.session.add(u)
+	db.session.commit(u)
+	flash('You are now following' + nickname)
+	return redirect(url_for('user', nickname = nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname = nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname = nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('user', nickname = nickname))
+
 
 
 '''
